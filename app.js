a68771e8ea8f5cfc9826e953e347cb837ebae9c1
Jake Overall <joverall22@gmail.com>
2018-11-01 10:29:20 -0600

    Brian

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Connected!')
+let snibley = {
+  name: "mr. snibley",
+  pets: 0,
+  moods: ["happy", "bitey"],
+  tolerance: 3,
+  images: ["https://i.pinimg.com/236x/38/3a/08/383a08912f8d07e97671d7b7bb14db0d--cat-birthday-birthday-memes.jpg", "https://static.boredpanda.com/blog/wp-content/uploads/2016/02/japanese-grumpy-cat-angry-koyuki-moflicious-22.jpg"]
+}
+
+function pet() {
+  snibley.pets++
+}
+
+function reset() {
+  snibley.pets = 0
+}
+
+function draw() {
+
+
+}       
